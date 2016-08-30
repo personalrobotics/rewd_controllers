25a902340ff2b9c2c58c3cc125723de69f3f6ae2
Clint Liddick <clint.liddick@gmail.com>
2016-08-30 14:27:56 -0400

    refactor gravity compensation to use new helpers

--- include/rewd_controllers/gravity_compensation_controller.h
+++ include/rewd_controllers/gravity_compensation_controller.h
@@ -1,57 +1,54 @@
 #ifndef REWD_CONTROLLERS__GRAVITY_COMPENSATION_CONTROLLER_H
 #define REWD_CONTROLLERS__GRAVITY_COMPENSATION_CONTROLLER_H
 
-#include <boost/scoped_ptr.hpp>
-#include <boost/thread/condition.hpp>
+#include <memory>
 #include <controller_interface/multi_interface_controller.h>
-#include <hardware_interface/joint_command_interface.h>
-#include <hardware_interface/joint_state_interface.h>
-#include <fstream>
-#include <ros/node_handle.h>
 #include <dart/dynamics/dynamics.hpp>
-#include <unordered_map>
+#include <ros/node_handle.h>
+#include "helpers.hpp"
 
-namespace rewd_controllers {
-
-  class GravityCompensationController
-    : public controller_interface::MultiInterfaceController<hardware_interface::EffortJointInterface,
-                                                            hardware_interface::JointStateInterface> {
+namespace rewd_controllers
+{
+class GravityCompensationController
+    : public controller_interface::
+          MultiInterfaceController<hardware_interface::EffortJointInterface,
+                                   hardware_interface::JointStateInterface>
+{
 public:
+  GravityCompensationController();
+  virtual ~GravityCompensationController();
 
-    GravityCompensationController();
-    virtual ~GravityCompensationController();
+  /** \brief The init function is called to initialize the controller from a
+   * non-realtime thread with a pointer to the hardware interface, itself,
+   * instead of a pointer to a RobotHW.
+   *
+   * \param robot The specific hardware interface used by this controller.
+   *
+   * \param n A NodeHandle in the namespace from which the controller
+   * should read its configuration, and where it should set up its ROS
+   * interface.
+   *
+   * \returns True if initialization was successful and the controller
+   * is ready to be started.
+   */
+  bool init(hardware_interface::RobotHW* robot, ros::NodeHandle& n);
 
-    /** \brief The init function is called to initialize the controller from a
-     * non-realtime thread with a pointer to the hardware interface, itself,
-     * instead of a pointer to a RobotHW.
-     *
-     * \param robot The specific hardware interface used by this controller.
-     *
-     * \param n A NodeHandle in the namespace from which the controller
-     * should read its configuration, and where it should set up its ROS
-     * interface.
-     *
-     * \returns True if initialization was successful and the controller
-     * is ready to be started.
-     */
-    bool init(hardware_interface::RobotHW *robot, ros::NodeHandle &n);
+  /*!
+   * \brief Issues commands to the joint. Should be called at regular intervals
+   */
+  void update(const ros::Time& time, const ros::Duration& period);
 
-    /*!
-     * \brief Issues commands to the joint. Should be called at regular intervals
-     */
-    void update(const ros::Time& time, const ros::Duration& period);
+private:
+  JointAdapterFactory mAdapterFactory;
+  std::vector<std::unique_ptr<JointAdapter>> mAdapters;
 
-  private:
-    // Model
-    dart::dynamics::SkeletonPtr skeleton_;
-    std::vector<hardware_interface::JointStateHandle> joint_state_handles_;
+  dart::dynamics::SkeletonPtr mSkeleton;
+  dart::dynamics::MetaSkeletonPtr mControlledSkeleton;
+  std::unique_ptr<SkeletonJointStateUpdater> mSkeletonUpdater;
+  std::vector<hardware_interface::JointHandle> mControlledJointHandles;
+  Eigen::VectorXd mCalculatedForces;
+};
 
-    dart::dynamics::GroupPtr controlled_skeleton_;
-    std::vector<hardware_interface::JointHandle> controlled_joint_handles_;
-    std::unordered_map<std::string, size_t> controlled_joint_map_;
-
-  };
-
-} // namespace
+}  // namespace
 
 #endif
--- src/gravity_compensation_controller.cpp
+++ src/gravity_compensation_controller.cpp
@@ -1,148 +1,107 @@
 #include <rewd_controllers/gravity_compensation_controller.h>
+
 #include <hardware_interface/hardware_interface.h>
 #include <pluginlib/class_list_macros.h>
 #include <dart/dynamics/dynamics.hpp>
 #include <dart/utils/urdf/DartLoader.hpp>
 #include <aikido/util/CatkinResourceRetriever.hpp>
 
-namespace rewd_controllers {
-
-using EffortJointInterface = hardware_interface::EffortJointInterface;
-using JointStateInterface = hardware_interface::JointStateInterface;
-
+namespace rewd_controllers
+{
+//=============================================================================
 GravityCompensationController::GravityCompensationController() {}
 
+//=============================================================================
 GravityCompensationController::~GravityCompensationController() {}
 
-bool GravityCompensationController::init(
-  hardware_interface::RobotHW *robot, ros::NodeHandle &n)
+//=============================================================================
+bool GravityCompensationController::init(hardware_interface::RobotHW *robot,
+                                         ros::NodeHandle &n)
 {
-  // Load the URDF XML from the parameter server.
-  std::string robot_description_parameter;
-  n.param<std::string>("robot_description_parameter",
-    robot_description_parameter, "/robot_description");
-
-  std::string robot_description;
-  if (!n.getParam(robot_description_parameter, robot_description)) {
-    ROS_ERROR("Failed loading URDF from '%s' parameter.",
-              robot_description_parameter.c_str());
-    return false;
-  }
-
-  // Load the URDF as a DART model.
-  auto const resource_retriever
-    = std::make_shared<aikido::util::CatkinResourceRetriever>();
-  const dart::common::Uri base_uri{};
-
-  ROS_INFO("Loading DART model from URDF...");
-  dart::utils::DartLoader urdf_loader;
-  skeleton_ = urdf_loader.parseSkeletonString(
-    robot_description, base_uri, resource_retriever);
-  if (!skeleton_) {
-    ROS_ERROR("Failed loading '%s' parameter URDF as a DART Skeleton.",
-              robot_description_parameter.c_str());
-    return false;
-  }
-  ROS_INFO("Loading DART model from URDF...DONE");
+  using hardware_interface::JointStateInterface;
+  using hardware_interface::EffortJointInterface;
 
   // Build up the list of controlled DOFs.
-  ROS_INFO("Getting joint names");
-  std::vector<std::string> dof_names;
-  if (!n.getParam("joints", dof_names)) {
-    ROS_ERROR("Unable to read controlled DOFs from the parameter '%s/joints'.",
-      n.getNamespace().c_str());
+  const auto jointParameters = loadJointsFromParameter(n, "joints", "effort");
+  if (jointParameters.empty()) return false;
+
+  ROS_INFO_STREAM("Controlling " << jointParameters.size() << " joints:");
+  for (const auto &param : jointParameters) {
+    ROS_INFO_STREAM("- " << param.mName << " (type: " << param.mType << ")");
+
+    if (param.mType != "effort") {
+      ROS_ERROR_STREAM("Joint '"
+                       << param.mName
+                       << "' is not effort-controlled and cannot be "
+                          "used in a gravity compensation controller");
+      return false;
+    }
+  }
+
+  // Load the URDF as a Skeleton.
+  mSkeleton = loadRobotFromParameter(n, "robot_description_parameter");
+  if (!mSkeleton) return false;
+
+  // Extract the subset of the Skeleton that is being controlled.
+  mControlledSkeleton =
+      getControlledMetaSkeleton(mSkeleton, jointParameters, "Controlled");
+  if (!mControlledSkeleton) return false;
+
+  // the full skeleton.
+  const auto jointStateInterface = robot->get<JointStateInterface>();
+  if (!jointStateInterface) {
+    ROS_ERROR("Unable to get JointStateInterface from RobotHW instance.");
     return false;
   }
 
-  ROS_INFO("Creating controlled Skeleton");
-  controlled_skeleton_ = dart::dynamics::Group::create("controlled");
-  for (std::string const &dof_name : dof_names) {
-    dart::dynamics::DegreeOfFreedom *const dof = skeleton_->getDof(dof_name);
-    if (!dof) {
-      ROS_ERROR("There is no DOF named '%s'.", dof_name.c_str());
-      return false;
-    }
-    controlled_skeleton_->addDof(dof, true);
+  mSkeletonUpdater.reset(
+      new SkeletonJointStateUpdater{mSkeleton, jointStateInterface});
 
-    controlled_joint_map_.emplace(
-      dof->getName(), controlled_skeleton_->getNumDofs() - 1);
+  const auto numControlledDofs = mControlledSkeleton->getNumDofs();
+
+  mControlledJointHandles.resize(numControlledDofs);
+  mCalculatedForces.resize(numControlledDofs);
+
+  const auto effortJointInterface = robot->get<EffortJointInterface>();
+  if (!effortJointInterface) {
+    ROS_ERROR("Unable to get EffortJointInterface from RobotHW instance.");
+    return false;
   }
 
-  // Get all joint handles.
-  ROS_INFO("Getting controlled JointHandles");
-  EffortJointInterface *ei = robot->get<EffortJointInterface>();
-  controlled_joint_handles_.reserve(controlled_skeleton_->getNumDofs());
-  for (dart::dynamics::DegreeOfFreedom const *const dof : controlled_skeleton_->getDofs()) {
-    std::string const &dof_name = dof->getName();
-    hardware_interface::JointHandle handle;
-
+  for (size_t idof = 0; idof < numControlledDofs; ++idof) {
+    const auto dofName = mControlledSkeleton->getDof(idof)->getName();
     try {
-      handle = ei->getHandle(dof_name);
-    } catch (hardware_interface::HardwareInterfaceException const &e) {
-      ROS_ERROR_STREAM("Failed getting JointHandle for controlled DOF '"
-                       << dof_name.c_str() << "'. "
-                       << "Joint will be treated as if always in default "
-                       << "position, velocity, and acceleration.");
+      auto handle = effortJointInterface->getHandle(dofName);
+      mControlledJointHandles[idof] = handle;
+    } catch (const hardware_interface::HardwareInterfaceException &e) {
+      ROS_ERROR_STREAM(
+          "Unable to get interface of type 'EffortJointInterface' for joint '"
+          << dofName << "'.");
       return false;
     }
-
-    controlled_joint_handles_.push_back(handle);
-  }
-
-  ROS_INFO("Getting all JointStateHandles");
-  JointStateInterface *jsi = robot->get<JointStateInterface>();
-  joint_state_handles_.reserve(skeleton_->getNumDofs());
-  for (dart::dynamics::DegreeOfFreedom const *const dof : skeleton_->getDofs()) {
-    std::string const &dof_name = dof->getName();
-    hardware_interface::JointStateHandle handle;
-
-    try {
-      handle = jsi->getHandle(dof_name);
-    } catch (hardware_interface::HardwareInterfaceException const &e) {
-      ROS_WARN_STREAM("Failed getting JointStateHandle for read-only DOF '"
-                       << dof_name.c_str() << "'. "
-                       << "Joint will be treated as if always in default "
-                       << "position, velocity, and acceleration.");
-      continue;
-    }
-
-    joint_state_handles_.push_back(handle);
   }
 
   ROS_INFO("GravityCompensationController initialized successfully");
   return true;
 }
 
-void GravityCompensationController::update(
-  const ros::Time& time, const ros::Duration& period)
+//=============================================================================
+void GravityCompensationController::update(const ros::Time &time,
+                                           const ros::Duration &period)
 {
-  for (hardware_interface::JointStateHandle &handle : controlled_joint_handles_) {
-    dart::dynamics::DegreeOfFreedom *const dof
-      = skeleton_->getDof(handle.getName());
-    if (!dof)
-      continue; // This should never happen.
+  mSkeletonUpdater->update();
 
-    dof->setPosition(handle.getPosition());
-    dof->setVelocity(handle.getVelocity());
-    dof->setAcceleration(0.);
-  }
+  // Compute inverse dynamics torques for the current configuration
+  mSkeleton->computeInverseDynamics();
+  mCalculatedForces = mControlledSkeleton->getForces();
 
-  skeleton_->computeInverseDynamics();
-
-  for (size_t i = 0; i < controlled_skeleton_->getNumDofs(); ++i) {
-    dart::dynamics::DegreeOfFreedom *const dof
-      = controlled_skeleton_->getDof(i);
-
-    double const effort_inversedynamics = dof->getForce();
-
-    hardware_interface::JointHandle &joint_handle
-      = controlled_joint_handles_[i];
-    joint_handle.setCommand(effort_inversedynamics);
+  for (size_t idof = 0; idof < mControlledJointHandles.size(); ++idof) {
+    auto jointHandle = mControlledJointHandles[idof];
+    jointHandle.setCommand(mCalculatedForces[idof]);
   }
 }
 
-} // namespace rewd_controllers
+}  // namespace rewd_controllers
 
-PLUGINLIB_EXPORT_CLASS(
-  rewd_controllers::GravityCompensationController,
-  controller_interface::ControllerBase)
+PLUGINLIB_EXPORT_CLASS(rewd_controllers::GravityCompensationController,
+                       controller_interface::ControllerBase)

71a5cb0215994a358b27d36b3934bb974d83e470
Ethan Gordon <ekgordon@cs.washington.edu>
2019-08-21 14:45:59 -0700

    Update travis for Ubuntu bionic

--- .ci/install.sh
+++ .ci/install.sh
@@ -2,6 +2,9 @@
 
 set -ex
 
-cd "${HOME}/workspace"
-cp -r "${TRAVIS_BUILD_DIR}" src
-./scripts/internal-distro.py --workspace=src distribution.yml --repository "${REPOSITORY}"
+if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
+  if [ "${USE_CATKIN}" = "ON" ]; then
+    . "${TRAVIS_BUILD_DIR}/.ci/install_linux_catkin.sh"
+  fi
+fi
+
